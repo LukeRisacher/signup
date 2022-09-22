ec855436300c621ec48226ebd3665d839fce4be4
Antonio <antonioabela3609@gmail.com>
2022-09-22 00:12:44 -0400

    made everything better
    Co-authored-by: Luke Risacher <Flitez@users.noreply.github.com>

--- archersignup/scheduler/views.py
+++ archersignup/scheduler/views.py
@@ -4,18 +4,35 @@ from django import forms
 from .forms import SignUpForm
 from .models import signup
 from django.contrib import messages
+import datetime as dt
+import json
+from .dayFormat import get_day_value
+from pprint import pprint
 
 def home(request):
     return HttpResponse('Home') 
 
 def all_signups(request):
     signup_list = signup.objects.all()
-    return render(request, 'scheduler/all_signups.html', {'signup_list': signup_list})
+    displaydays = {"mon":[], "tue":[], "wed":[], "thu":[], "fri":[], "sat":[], "sun":[]}
+    for day in signup_list:
+        day_info = []
+        day_info.extend([day.Pick_Time, day.Name])
+        result = get_day_value(day.Pick_Day)
+        if result is not None:
+            displaydays[result].append(day_info)
+            print(displaydays)
+            #print(displaydays['wed'])
+
+        #print(result)
+
+    return render(request, 'scheduler/all_signups.html', {'signup_list': signup_list, 'displaydays': displaydays})
     
 def regpage(request):
     if request.POST:
         form = SignUpForm(request.POST)
         signup_list = signup.objects.all()
+        day = dt.date(int(request.POST['Pick_Day'][0:4]), int(request.POST['Pick_Day'][5:7]), int(request.POST['Pick_Day'][8:]))
         if form.is_valid:
             x = 0
             for object in signup_list:
@@ -24,6 +41,7 @@ def regpage(request):
                     messages.warning(request, 'The selected time slot is already filled.')
             if x == 0:
                 form.save()
+
                 return redirect(all_signups)   
         return HttpResponseRedirect("/")
     return render(request, 'scheduler/signup.html', {'form': SignUpForm})
