633e67ae8087983a43ee130154c6ac17cfa76573
Antonio Abela <57450592+AntonioAbela@users.noreply.github.com>
2022-09-22 18:45:51 -0400

    Cleaned up get_day_value

--- archersignup/scheduler/dayFormat.py
+++ archersignup/scheduler/dayFormat.py
@@ -18,14 +18,11 @@ def dateFinder():
 day = dt.date(2022, 9, 21)
 
 def get_day_value(day):
-    
     dayValue = {'mon': '0', 'tue': '1', 'wed': '2', 'thu': '3', 'fri': '4', 'sat': '5', 'sun': '6',}
     valueDay = {'0': 'mon', '1': 'tue', '2': 'wed', '3': 'thu', '4': 'fri', '5': 'sat', '6': 'sun',}
     today = dt.date.today()
-    today_week = today.isocalendar()
-    today_week = today_week.week
-    event_week = day.isocalendar()
-    event_week = event_week.week
+    today_week = today.isocalendar().week
+    event_week = day.isocalendar().week
     if today_week == event_week:
        theday = day.weekday()
        for i in dayValue:
